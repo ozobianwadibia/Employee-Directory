c22bac3b7aad8846603e846d7814e65eebb87d0f
Ozobia Nwadibia <Green_energy@outlook.com>
2018-09-22 15:10:29 -0500

    Created view function in app.js

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,3 +1,16 @@
+//This function makes the employee list visible after the 'view' button is pressed
 function view() {
+    $(".main-body").empty();
+    for (let i = 0; i < employeeList.length; i++) {
+        $(".main-body").append("<div class='employeeInfo'>" + "<p>" + employeeList[i].name + "</p>" +
+            "<p>" + employeeList[i].officeNum + "</p>" +
+            "<p>" + employeeList[i].phoneNum + "</p>" + "</div");
+    }
+}
+$("#view").on("click", view);
+
+//This function adds to the employee list after the 'add' button is pressed
+function add() {
+
 
 }
