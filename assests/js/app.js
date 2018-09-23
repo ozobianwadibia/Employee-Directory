840e7a92f28ebb1de2e1982cbfe15099771b2315
Ozobia Nwadibia <Green_energy@outlook.com>
2018-09-22 20:30:58 -0500

    Added the 'add' function in app.js

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,6 +1,6 @@
 //This function makes the employee list visible after the 'view' button is pressed
 function view() {
-    $(".main-body").empty();
+    // $(".main-body").empty();
     for (let i = 0; i < employeeList.length; i++) {
         $(".main-body").append("<div class='employeeInfo'>" + "<p>" + employeeList[i].name + "</p>" +
             "<p>" + employeeList[i].officeNum + "</p>" +
@@ -9,8 +9,29 @@ function view() {
 }
 $("#view").on("click", view);
 
+function changeColor() {
+    document.getElementById("view").style.backgroundColor = "black";
+    document.getElementById("view").style.color = "white";
+}
+$("#view").on("click", changeColor);
+
 //This function adds to the employee list after the 'add' button is pressed
 function add() {
+    const nameInput = $("#nameInput").val();
+    const officenumInput = parseInt($("#officenumInput").val());
+    const phonenumInput = $("#phonenumInput").val();
 
+    // if (input has field, execute code below
+    //     else ignore!!!) {
 
+    // }
+    //finish later
+    employeeList.push({ name: nameInput, officeNum: officenumInput, phoneNum: phonenumInput });
+
+    // After performing our actions, clear the name input and re-render the list
+    $("#nameInput").val('');
+    $("#officenumInput").val('');
+    $("#phonenumInput").val('');
+
+    view();
 }
